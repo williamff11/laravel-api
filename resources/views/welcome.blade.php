5ac175d489d2f63aa78697bca7aee11b333682d6
William Felicio Freire <william.freire@credisis.local>
2020-01-02 15:22:20 -0300

    ativando o vuejs

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -64,7 +64,7 @@
         </style>
     </head>
     <body>
-        <div class="flex-center position-ref full-height">
+        <div id="app" class="flex-center position-ref full-height">
             @if (Route::has('login'))
                 <div class="top-right links">
                     @auth
@@ -96,5 +96,7 @@
                 </div>
             </div>
         </div>
+
+        <script src="{{ mix('./js/app.js') }}"></script>
     </body>
 </html>
